6458e089f68488ed324a1c3b071b3f041b2f276a
MutantAura <domw0401@gmail.com>
2023-12-21 16:06:39 +0000

    Merge diassembler into core cpu emulation loop.

--- src/fish.c
+++ src/fish.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
+// System constants
 #define MAX_MEMORY 4096
+#define STACK_SIZE 16
+
+// Memory locations
 #define ROM_START 0x200
 #define DISPLAY_START 0xF00
 
@@ -10,9 +14,10 @@ typedef struct {
     // 4Kb of main system memory organised in a byte array.
     uint8_t memory[MAX_MEMORY];
 
-    // Visual display of 64x32 resolution (monochrome).
+    // Visual display of 64x32 resolution (monochrome). Display is located at 0xF00 of memory.
     // TODO: Replace with "Pixel" type?
     uint8_t* display;
+
     // Program counter is a pointer to the current instruction to execute.
     uint16_t pc;
 
@@ -20,7 +25,7 @@ typedef struct {
     uint16_t i_reg;
 
     // Stack. Can store up to 16, 16-bit addresses in LIFO fashion.
-    uint16_t stack[16];
+    uint16_t stack[STACK_SIZE];
 
     // Stack Pointer. Points to top most level of the stack.
     uint16_t sp;
@@ -52,6 +57,9 @@ typedef struct {
 
     // CPU exectution speed (instructions per second to execute).
     uint8_t frequency;
+
+    // Kill option
+    int exit_requested;
 } Fish;
 
 uint8_t font_array[] = {
@@ -76,6 +84,7 @@ uint8_t font_array[] = {
 void InitFish(Fish* state) {
     state->display = &state->memory[DISPLAY_START];
     state->pc = ROM_START;
+    state->exit_requested = 0;
 }
 
 int LoadRom(char* file_name, uint8_t* memory) {
@@ -94,6 +103,82 @@ int LoadRom(char* file_name, uint8_t* memory) {
     return 0;
 }
 
+void EmulateCpu(Fish* device) {
+    uint8_t* current_instr;
+    uint8_t instr_nib[4];
+
+    while (!device->exit_requested) {
+        // Extract opcode data and break down into nibbles.
+        current_instr = &device->memory[device->pc];
+        instr_nib[0] = current_instr[0] >> 4; 
+        instr_nib[1] = current_instr[0] & 0x0F; 
+        instr_nib[2] = current_instr[1] >> 4; 
+        instr_nib[3] = current_instr[1] & 0x0F;
+
+        // Opcodes from http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#8xy0
+        // Some name changes?
+        switch(instr_nib[0]) {
+            case 0x00: {
+                switch (current_instr[1]) {
+                    case 0xE0: printf("%-10s\n", "CLS"); break;
+                    case 0xEE: printf("%-10s\n", "RET"); break;
+                    default: printf("%-10s $%01x%01x%01x\n", "SYS", instr_nib[1], instr_nib[2], instr_nib[3]);
+                }
+            } break;
+            case 0x1: printf("%-10s $%01x%01x%01x\n", "JMP", instr_nib[1], instr_nib[2], instr_nib[3]); break;
+            case 0x2: printf("%-10s $%01x%01x%01x\n", "CALL", instr_nib[1], instr_nib[2], instr_nib[3]); break;
+            case 0x3: printf("%-10s V%01x, #$%02x\n", "SKIP.CMP", instr_nib[1], current_instr[1]); break;
+            case 0x4: printf("%-10s V%01x, #$%02x\n", "SKIP.NCMP", instr_nib[1], current_instr[1]); break;
+            case 0x5: printf("%-10s V%01x, V%01x\n", "SKIP.RCMP", instr_nib[1], instr_nib[2]); break;
+            case 0x6: printf("%-10s V%01X,#$%02x\n", "MVI", instr_nib[1], current_instr[1]); break;
+            case 0x7: printf("%-10s V%01X,#$%02x\n", "ADD", instr_nib[1], current_instr[1]); break;
+            case 0x8: {
+                switch (instr_nib[3]) {
+                    case 0x0: printf("%-10s V%01x,V%01x\n", "CPY", instr_nib[1], instr_nib[2]); break;
+                    case 0x1: printf("%-10s V%01x,V%01x\n", "OR", instr_nib[1], instr_nib[2]); break;
+                    case 0x2: printf("%-10s V%01x,V%01x\n", "AND", instr_nib[1], instr_nib[2]); break;
+                    case 0x3: printf("%-10s V%01x,V%01x\n", "XOR", instr_nib[1], instr_nib[2]); break;
+                    case 0x4: printf("%-10s V%01x,V%01x (VF)\n", "ADD", instr_nib[1], instr_nib[2]); break;
+                    case 0x5: printf("%-10s V%01x,V%01x\n", "SUB", instr_nib[1], instr_nib[2]); break;
+                    case 0x6: printf("%-10s V%01x,V%01x (VF)\n", "SHR", instr_nib[1], instr_nib[2]); break;
+                    case 0x7: printf("%-10s V%01x,V%01x (VF)\n", "SUBN", instr_nib[1], instr_nib[2]); break;
+                    case 0xE: printf("%-10s V%01x,V%01x (VF)\n", "SHL", instr_nib[1], instr_nib[2]); break;
+                    default: puts("Unknown `8` opcode."); break;
+                }
+            } break;
+            case 0x9: printf("%-10s V%01x, V%01x\n", "SNE", instr_nib[1], instr_nib[2]); break;
+            case 0xa: printf("%-10s I,#$%01x%02x\n", "LDI", instr_nib[1], current_instr[1]); break;
+            case 0xb: printf("%-10s $%01x%02x + V0\n", "JMP.V", instr_nib[1], current_instr[1]); break;
+            case 0xc: printf("%-10s V%01x, #$%02x\n", "RAND", instr_nib[1], current_instr[1]); break;
+            case 0xd: printf("%-10s V%01x, V%01x bytes: %01d\n", "DRW", instr_nib[1], instr_nib[2], (int)instr_nib[3]); break;
+            case 0xe: {
+                switch (current_instr[1]) {
+                    case 0x9E: printf("%-10s V%01x\n", "SKIP.KEYX", instr_nib[1]); break;
+                    case 0xA1: printf("%-10s V%01x\n", "SKIPN.KEYX", instr_nib[1]); break;
+                    default: puts("Unknown `e` opcode.");
+                }
+            } break;
+            case 0xf: {
+                switch (current_instr[1]) {
+                    case 0x07: printf("%-10s V%01x, DT\n", "LDX.DT", instr_nib[1]); break;
+                    case 0x0A: printf("%-10s V%01x, KEY\n", "LDX.KEY", instr_nib[1]); break;
+                    case 0x15: printf("%-10s DT, V%01x\n", "LDDT.X", instr_nib[1]); break;
+                    case 0x18: printf("%-10s ST, V%01x\n", "LDST.X", instr_nib[1]); break;
+                    case 0x1E: printf("%-10s I, V%01x\n", "ADDI.X", instr_nib[1]); break;
+                    case 0x29: printf("%-10s I, Sprite: %01x\n", "LDI.FX", instr_nib[1]); break;
+                    case 0x33: printf("%-10s I, (BCD)V%01x\n", "LDB.X", instr_nib[1]); break;
+                    case 0x55: printf("%-10s I, V0 -> V%01x\n", "LDI.ALL", instr_nib[1]); break;
+                    case 0x65: printf("%-10s V0 -> V%01x, I\n", "LDX.ALL", instr_nib[1]); break;
+                    default: puts("Unknown `f` opcode."); break;
+                }
+            } break;
+        }
+        
+        // Increment PC by 2 after each instruction call.
+        device->pc += 2;
+    }
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         puts("you are stupid.");
@@ -104,11 +189,15 @@ int main(int argc, char** argv) {
     Fish state = {0};
     InitFish(&state);
 
-    // Load ROM file into state memory.
-    if (LoadRom(argv[1], &state.memory) != 0) {
+    // Load ROM file into device memory.
+    if (LoadRom(argv[1], &state.memory[0]) != 0) {
         puts("You are also stupid (file error)");
     }
-    // Enter SDL loop
+
+    // Start CPU cycles
+    EmulateCpu(&state);
+
+    // Enter SDL loop?
 
     // Cleanup
     return 0;
